33887dc068fcdebf16254d257624051044ae053b
Manoj-2301 <manojthapa.uiux@gmail.com>
2026-05-21 10:55:20 +0530

    new robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = (process.env.NEXT_PUBLIC_APP_URL || 'https://streamfinds.vercel.app').replace(/\/+$/, '');
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
